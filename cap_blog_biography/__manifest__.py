d107fb69057cd4b139c9286986cdc7862c0a783d
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-05-22 12:20:17 -0400

    Release Candidate Usabiity Testing

---
+++ cap_blog_biography/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cap_blog_biography",
+    'summary': """
+        Add Biography to partner""",
+    'description': """
+        Adds a Biography to the partner object. Shows this biography
+        under the partners picture when posting in a blog.
+    """,
+    'author': "Christopher Beirne Captivea",
+    'website': "https://www.Captivea.us/",
+    'category': 'Website',
+    'version': '0.1',
+    'depends': ['base','website','website_blog'],
+    'data': [
+        'views/blogpost.xml',
+        'views/views.xml',
+    ]
+}
