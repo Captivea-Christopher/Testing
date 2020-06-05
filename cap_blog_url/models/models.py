5f9e1869ffb2a52df84c8bb78f7b7fd44483f6bc
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-06-05 11:38:25 -0400

    Update models.py

--- cap_blog_url/models/models.py
+++ cap_blog_url/models/models.py
@@ -7,10 +7,9 @@ class my_module(models.Model):
     _inherit = 'blog.post'
 
     custom_url = fields.Text()    #no uppercase, gets changed by the system
+	_sql_constraints = [('custom_url_unique', 'unique(custom_url)', 'There cannot be duplicate custom URLs')]
 	
-	 _sql_constraints = [('custom_url_unique', 'unique(custom_url)', 'There cannot be duplicate custom URLs')]
-
-	    @api.multi
+	@api.multi
     def _compute_website_url(self):
         super(BlogPost, self)._compute_website_url()
         for blog_post in self:
