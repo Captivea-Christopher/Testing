5ab88f17d040a4e0c0f94bdac286e44e0c59e375
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-06-10 10:27:38 -0400

    Update portal_task_submission.py

--- cap_portal_task_submission/controllers/portal_task_submission.py
+++ cap_portal_task_submission/controllers/portal_task_submission.py
@@ -7,7 +7,7 @@ class portal_task_submission(http.Controller):
 	def index(self, **kw):
 		partner_id = request.env.user.partner_id.id
 		list_of_projects_owned_by_customer = request.env['project.project'].sudo().search([
-			('partner_id', 'child of', partner_id)
+			('partner_id', '=', partner_id)
 		])
 		return http.request.render('cap_portal_task_submission.portal_task_submission', {
 			'list_of_projects_owned_by_customer': list_of_projects_owned_by_customer,
