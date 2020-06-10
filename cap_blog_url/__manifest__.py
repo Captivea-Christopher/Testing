8aeadd0b364e560661ab38ac68acf99e60ca2d32
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-06-10 09:25:35 -0400

    Init

---
+++ cap_blog_url/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cap_blog_url",
+
+    'summary': """
+        Allows Custom Blog URL Paths""",
+
+    'description': """
+        Allows the creation of custom blog url for Captivea Site Blog
+    """,
+
+    'author': "Captivea USA",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','website_blog'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ cap_blog_url/models/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of CAPTIVEA. Odoo 12 EE.
+from odoo import models, fields, api
+from odoo.addons.http_routing.models.ir_http import slug
+
+class BlogPost(models.Model):
+	_inherit = 'blog.post'
+	
+	custom_url = fields.Text(default = record.name)      #no uppercase, gets changed by the system
+	_sql_constraints = [('custom_url_unique', 'unique(custom_url)', 'There cannot be duplicate custom URLs')]
+	
+	@api.multi
+	def _compute_website_url(self):
+		super(BlogPost, self)._compute_website_url()
+		for blog_post in self:
+			blog_post.website_url = "/odoo/%s" % (slug(blog_post.custom_url))
