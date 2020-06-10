493ffdc78c6593a3309597632c3061118b0f29cb
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-06-10 16:33:31 -0400

    Init

---
+++ cap_portal_task_submission/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cap_portal_task_submission",
+
+    'summary': """
+        Adds customer task submission page
+        """,
+
+    'description': """
+        Adds a new webpage which allows a customer to submit a task directly to a project.
+        Adds a button to the user portal that redirects to the page where tasks are submitted.
+    """,
+
+    'author': "Christopher Beirne Captivea",
+    'website': "https://www.Captivea.us/",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','website','project','contacts', 'website_form'],
+
+    # always loaded
+    'data': [
+        'views/templates.xml',
+        'data/data.xml',
+    ]
+}
---
+++ cap_portal_task_submission/controllers/portal_task_submission.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+from odoo.http import request
+
+class portal_task_submission(http.Controller):
+	@http.route('/my/submit_task', type='http', auth='user', website=True)
+	def index(self, **kw):
+		partner_id = request.env.user.partner_id.parent_id.id
+		list_of_projects_owned_by_customer = request.env['project.project'].sudo().search([])
+		projects_exist = list_of_projects_owned_by_customer.exists()
+		return http.request.render('cap_portal_task_submission.portal_task_submission', {
+			'list_of_projects_owned_by_customer': list_of_projects_owned_by_customer,
+			'projects_exist' : projects_exist
+			})
