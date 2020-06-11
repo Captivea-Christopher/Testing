bfa3f9763a8fc408d3cb7e7b676cb06dead509a9
Captivea-Christopher <64795176+Captivea-Christopher@users.noreply.github.com>
2020-06-11 14:46:08 -0400

    Update Manifests
    Update Manifests for Blog Bio and Task Submission.

--- cap_blog_biography/__manifest__.py
+++ cap_blog_biography/__manifest__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 {
     'name': "cap_blog_biography",
+    
     'summary': """
         Add Biography to partner""",
+        
     'description': """
         Adds a Biography to the partner object. This Biography
 	field is shown on the contact page of partners belonging
@@ -11,11 +13,16 @@
 	Shows this biography alongside the partners name and picture,
 	as well as the publishing date when posting a blogpost.
     """,
+    
     'author': "Christopher Beirne Captivea",
     'website': "https://www.Captivea.us/",
+    
     'category': 'Website',
-    'version': '1.0',
+    
+    'version': '0.2',
+    
     'depends': ['base','website','website_blog'],
+    
     'data': [
         'views/blogpost.xml',
         'views/views.xml',
--- cap_portal_task_submission/__manifest__.py
+++ cap_portal_task_submission/__manifest__.py
@@ -7,20 +7,26 @@
         """,
 
     'description': """
-        Adds a new webpage which allows a customer to submit a task directly to a project.
-        Adds a button to the user portal that redirects to the page where tasks are submitted.
+        Adds a new webpage which allows a customer to submit a task directly to a project
+        that belongs to the company that the customer belongs to. 
+        Updates the My Account Page to include a button, which only appears if that customer
+        belongs to a company which is the customer on at least one project.
+        This button links to a task submission page, which takes in a project (to link the new
+        task to), a Required task name, and a description of the task. Upon submission of the new task
+        the user will be redirected back to the "My Account" page.
+        
+        Note: This requires that all customers are correctly linked to their company through the
+        contact page (res.partner model).
     """,
 
     'author': "Christopher Beirne Captivea",
     'website': "https://www.Captivea.us/",
 
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
 
-    # any module necessary for this one to work correctly
     'depends': ['base','website','project','contacts', 'website_form'],
 
-    # always loaded
     'data': [
         'views/templates.xml',
         'data/data.xml',
